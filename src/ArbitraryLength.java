095c44904107c4c79432d2ad98b60e78b71c443c
Conterry <conterry05@gmail.com>
2021-11-20 01:40:36 +0200

    Added sum function

--- src/ArbitraryLength.java
+++ src/ArbitraryLength.java
@@ -1,31 +1,42 @@
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class ArbitraryLength {
+
+
+
     public List<Integer> arbitrary = new ArrayList<>();
 //    private int rank;
 
-    public ArbitraryLength(int value){
-        List<Integer> reversedArbitrary = new ArrayList<Integer>();
+
+
+    public ArbitraryLength(){}
+
+    public ArbitraryLength(List<Integer> value){
+        arbitrary = value;
+    }
+
+    public ArbitraryLength(Integer value){
+        List<Integer> ReversedArbitrary = new ArrayList<Integer>();
         while(value!=0){
-            reversedArbitrary.add((int)(value%10));
+            ReversedArbitrary.add((int)(value%10));
             value/=10;
         }
-        for(int i=reversedArbitrary.size()-1; i>=0; i++){
-            arbitrary.add(reversedArbitrary.get(i));
+        for(int i=ReversedArbitrary.size()-1; i>=0; i--){
+            arbitrary.add(ReversedArbitrary.get(i));
         }
     }
 
     public ArbitraryLength(long value){
-        List<Integer> reversedArbitrary = new ArrayList<Integer>();
+        List<Integer> ReversedArbitrary = new ArrayList<Integer>();
         while(value!=0){
-            reversedArbitrary.add((int)(value%10));
+            ReversedArbitrary.add((int)(value%10));
             value/=10;
         }
-        for(int i=reversedArbitrary.size()-1; i>=0; i++){
-            arbitrary.add(reversedArbitrary.get(i));
+        for(int i=ReversedArbitrary.size()-1; i>=0; i--){
+            arbitrary.add(ReversedArbitrary.get(i));
         }
-
     }
 
     public ArbitraryLength(String value){
@@ -42,8 +53,8 @@ public class ArbitraryLength {
     private Boolean checkIfStringIsNumber(String value){
 
         for(int i=0; i<value.length(); i++){
-            int currentChar = value.charAt(i);
-            if(currentChar<48 && currentChar>57){
+            int CurrentChar = value.charAt(i);
+            if(CurrentChar<48 && CurrentChar>57){
                 return false;
             }
         }
@@ -57,4 +68,68 @@ public class ArbitraryLength {
         }
     }
 
+    public int length(){
+        return this.arbitrary.size();
+    }
+
+    public ArbitraryLength sumOf(ArbitraryLength... arguments) {
+        if (arguments.length == 1) {
+            return arguments[0];
+        }
+        if (arguments.length == 2) {
+            return MakeSumOfTwo(arguments[0], arguments[1]);
+        } else {
+            ArbitraryLength TemporaryArgument = arguments[0];
+            for (int i = 0; i < arguments.length - 1; i++) {
+                TemporaryArgument = MakeSumOfTwo(TemporaryArgument, arguments[i + 1]);
+            }
+            return TemporaryArgument;
+        }
+    }
+
+    public ArbitraryLength MakeSumOfTwo(ArbitraryLength first, ArbitraryLength second) {
+        int MinLength = Math.min(first.length(), second.length());
+        int MaxLength = Math.max(first.length(), second.length());
+        int Delta = MaxLength - MinLength;
+        List<Integer> ReversedFirst = first.arbitrary;
+        Collections.reverse((ReversedFirst));
+        List<Integer> ReversedSecond = second.arbitrary;
+        Collections.reverse((ReversedSecond));
+        int Adder = 0;
+
+        if(MinLength == first.length()){
+            for(int i=0; i<Delta; i++){
+                ReversedFirst.add(0);
+            }
+        }
+        else {
+            for(int i=0; i<Delta; i++){
+                ReversedSecond.add(0);
+            }
+        }
+
+        List<Integer> newReversedArbitrary = new ArrayList<Integer>();
+        for(int i=0; i<MaxLength; i++){
+            newReversedArbitrary.add(ReversedFirst.get(i) + ReversedSecond.get(i));
+        }
+
+        for (int i=0; i<MaxLength; i++){
+            int CurrentValue = newReversedArbitrary.get(i);
+            if(Adder == 1){
+                CurrentValue += 1;
+                Adder = 0;
+            }
+            if(CurrentValue>=10){
+                Adder = 1;
+                CurrentValue-=10;
+            }
+            newReversedArbitrary.set(i, CurrentValue);
+        }
+        if(Adder == 1){
+            newReversedArbitrary.add(1);
+        }
+
+        Collections.reverse(newReversedArbitrary);
+        return new ArbitraryLength(newReversedArbitrary);
+    }
 }
--- src/MainClass.java
+++ src/MainClass.java
@@ -1,6 +1,14 @@
+import java.util.ArrayList;
+
 public class MainClass {
     public static void main(String[] args) {
-        var value = new ArbitraryLength("49071247914793834717278");
-        value.ToString();
+
+        var some = new ArbitraryLength("10");
+        var some2 = new ArbitraryLength("190");
+        var some3 = new ArbitraryLength("800");
+        var some4 = new ArbitraryLength(9000);
+        var sum = new ArbitraryLength().sumOf(some, some2, some3, some4);
+        sum.ToString();
+
     }
 }
